1652106f51d93b9f892e5008cd0e026f04ac8307
marinacabalzarr <marinacabalzar@gmail.com>
2025-02-24 11:18:46 -0300

    codigo curso

--- Project/Aplicação/src/Disciplina.java
+++ Project/Aplicação/src/Disciplina.java
@@ -1,12 +1,35 @@
 public class Disciplina {
     private String nome;
     private int numeroAlunos;
-
+    public Disciplina(String nome) {
+        this.nome = nome;
+        this.numeroAlunos = 0;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getNumeroAlunos() {
+        return numeroAlunos;
+    }
+    public void matricularAluno() {
+        if (numeroAlunos < 60) {
+            numeroAlunos++;
+        } else {
+            System.out.println("Matrículas encerradas para a disciplina: " + nome);
+        }
+    }
+    public void cancelarMatricula() {
+        if (numeroAlunos > 0) {
+            numeroAlunos--;
+        }
+    }
     public boolean isObrigatoria(){
         return true;
     }
-
     public boolean verificaAtividade(){
-        return true;
+        return numeroAlunos >= 3;
     }
 }
