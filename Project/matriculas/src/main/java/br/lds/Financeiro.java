1a5f8760f047fc2f863667756845344ac35c225d
andrexlazarini <144286243+andrexlazarini@users.noreply.github.com>
2025-03-23 19:23:14 -0300

    Delete Project directory

--- Project/matriculas/src/main/java/br/lds/Financeiro.java
+++
@@ -1,8 +0,0 @@
-package br.lds;
-
-public class Financeiro {
-
-    public void cobrarAluno(Aluno aluno, Disciplina disciplina) {
-        System.out.println("Cobrança realizada para disciplina: " + disciplina.getNome());
-    }
-}
