b5849bab43425ff978053e5c9c4a64a9ea9ea5a9
andrexlazarini <1447036@sga.pucminas.br>
2025-04-27 17:24:15 -0300

    Deletando antigo

--- demo/src/main/java/br/lds/demo/repository/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package br.lds.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.lds.demo.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- demo/src/main/java/br/lds/demo/repository/ContratoCreditoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.lds.demo.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.lds.demo.model.ContratoCredito;
-
-public interface ContratoCreditoRepository extends JpaRepository<ContratoCredito, Long> {
-}
